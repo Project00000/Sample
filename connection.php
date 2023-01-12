007f58299b033b4970395d928d824061833dd9ff
Project00000 <122522113+Project00000@users.noreply.github.com>
2023-01-12 01:58:51 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,3 @@
 <?php
-	$conn=mysqli_connect('fastdeal002.mysql.guardedhost.com
-','fastdeal002_dtsdb
-','Jomarmata','users');
+	$conn=mysqli_connect('fastdeal002.mysql.guardedhost.com','fastdeal002_dtsdb','Jomarmata','users');
 ?>
